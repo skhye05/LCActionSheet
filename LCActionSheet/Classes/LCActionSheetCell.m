cc4e5429cc481a8b96aee6bc33b2b22f74de1f2f
Jonathan Mayunga <mayunga.j@gmail.com>
2019-03-26 16:18:01 +0200

    Update LCActionSheetCell.m

--- LCActionSheet/Classes/LCActionSheetCell.m
+++ LCActionSheet/Classes/LCActionSheetCell.m
@@ -67,8 +67,12 @@
         self.titleLabel = titleLabel;
         [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.edges.equalTo(self.contentView).insets(self.buttonEdgeInsets);
+            make.right.equalTo(self).offset(-15);
         }];
         
+        UILabel *titleLabl = [[UILabel alloc] init];
+        titleLabl.text = "@MOI";
+            
         UIView *lineView = [[UIView alloc] init];
         lineView.backgroundColor = self.cellSeparatorColor;
         lineView.contentMode   = UIViewContentModeBottom;
