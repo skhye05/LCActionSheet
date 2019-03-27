ff99503b598868c4c82f6dd7ebee9278d7d03613
Jonathan Mayunga <mayunga.j@gmail.com>
2019-03-27 11:24:25 +0200

    Update LCActionSheet.m

--- LCActionSheet/Classes/LCActionSheet.m
+++ LCActionSheet/Classes/LCActionSheet.m
@@ -355,7 +355,7 @@
     [bottomView addSubview:titleLabel];
     [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(bottomView).offset(self.title.length > 0 ? self.titleEdgeInsets.top : 0);
-        make.left.equalTo(bottomView).offset(30);
+        make.left.equalTo(bottomView).offset(80);
         make.right.equalTo(bottomView).offset(-self.titleEdgeInsets.right);
         
         CGFloat height = self.title.length > 0 ? self.titleTextSize.height + 2.0f : 0;  // Prevent omit
